906837317619a46d66908e943b0d74a430fcd0ca
matiss-norenbergs <97153551+matiss-norenbergs@users.noreply.github.com>
2022-03-22 11:46:54 +0200

    More visual changes + animations

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -2,20 +2,25 @@ import './Recipe.css';
 import useFetch from '../../hooks/useFetch';
 import { useNavigate, useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { db } from "../../firebase/firebase";
 import { deleteDoc, doc } from "firebase/firestore";
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 
 const Recipe = () => {
     const { id } = useParams();
     const navigate = useNavigate();
     const { data: recipe, error, isPending } = useFetch("recipes", id);
     const user = useSelector(state => state.user.user);
+    const [showRecipeCard, setShowRecipeCard] = useState(true);
 
-    const deleteRecipe = async () => {
-        await deleteDoc(doc(db, "recipes", id));
-        navigate("/");
+    const deleteRecipe = () => {
+        setShowRecipeCard(false);
+        setTimeout( async () => {
+            await deleteDoc(doc(db, "recipes", id));
+            navigate("/");
+        }, 1400);
     }
 
     const isAuthor = () => {
@@ -26,26 +31,45 @@ const Recipe = () => {
         )
     }
 
+    const showRecipe = () => {
+        return (
+            <>
+                { error && <div className='stateInfo'>{ error }</div> }
+                { isPending && <div className='stateInfo'>Loading...</div> }
+                { recipe && (
+                    <article className="recipeInfo">
+                        <h1>{ recipe.title }</h1>
+                        <p className="cookTime">
+                            Takes { recipe.cookingTime } to cook.
+                            <span>
+                                {recipe.ingredients.map(ingredient => ingredient+", ")}
+                            </span>
+                        </p>
+
+                        <p className="cookMethod">{ recipe.method }</p>
+                        <p className="madeBy">Added by <span className="author">{ recipe.authorName }</span></p>
+
+                        { user && recipe.authorID === user.id ? isAuthor() : '' }
+                    </article>
+                )}
+            </>
+        )
+    }
+
+    const deletedRecipe = () => {
+        return (
+            <div className="recipeInfo">
+                <h1>Recipe deleted</h1>
+                <div className="faCheck">
+                    <FontAwesomeIcon icon={faCheck} />
+                </div>
+            </div>
+        )
+    }
+
     return(
         <div className="recipe">
-            { error && <div className='stateInfo'>{ error }</div> }
-            { isPending && <div className='stateInfo'>Loading...</div> }
-            { recipe && (
-                <article className="recipeInfo">
-                    <h1>{ recipe.title }</h1>
-                    <p className="cookTime">
-                        Takes { recipe.cookingTime } to cook.
-                        <span>
-                            {recipe.ingredients.map(ingredient => ingredient+", ")}
-                        </span>
-                    </p>
-
-                    <p className="cookMethod">{ recipe.method }</p>
-                    <p className="madeBy">Added by <span className="author">{ recipe.authorName }</span></p>
-
-                    { user && recipe.authorID === user.id ? isAuthor() : '' }
-                </article>
-            )}
+            { showRecipeCard === true ? showRecipe() : deletedRecipe() }
         </div>
     );
 }
